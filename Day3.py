af740cd6436d5a0af06d52c95da3719a202d7612
xLillium <nicolas.motillon@gmail.com>
2016-01-30 22:17:35 +0100

    Day 3 Part 2

--- Day3.py
+++ Day3.py
@@ -1,16 +1,52 @@
 # Day Two AoC : http://adventofcode.com/day/3
-location = [0, 0]
-locationList = {"0,0"}
+# Part 1 :
+def part1():
+    location = [0, 0]
+    locationSet = {"0,0"}
 
-directions = input("What are the directions for Santa?")
-for direction in directions:
-    if direction == ">":
-        location[0]+=1
-    if direction == "<":
-        location[0]-=1
-    if direction == "^":
-        location[1]+=1
-    if direction == "v":
-        location[1]-=1
-    locationList.add(str(location[0]) + "," + str(location[1]))
-print(len(locationList))
+    directions = input("What are the directions for Santa?")
+    for direction in directions:
+        if direction == ">":
+            location[0]+=1
+        if direction == "<":
+            location[0]-=1
+        if direction == "^":
+            location[1]+=1
+        if direction == "v":
+            location[1]-=1
+        locationSet.add(str(location[0]) + "," + str(location[1]))
+    print("Santa visited at least once : ",len(locationSet)," houses !")
+
+# Part 2 :
+def part2():
+    santaLocation = [0,0]
+    roboLocation = [0,0]
+    locationSet = {"0,0"}
+    turn = 0
+    tempLocation = [0,0]
+
+    directions = input("What are the directions for Santa?")
+    for direction in directions:
+        turn += 1
+        if direction == ">":
+            tempLocation[0]+=1
+        if direction == "<":
+            tempLocation[0]-=1
+        if direction == "^":
+            tempLocation[1]+=1
+        if direction == "v":
+            tempLocation[1]-=1
+        locationSet.add(str(tempLocation[0]) + "," + str(tempLocation[1]))
+        if turn % 2 == 1:
+            santaLocation = tempLocation
+            tempLocation = roboLocation
+        elif turn % 2 == 0:
+            roboLocation = tempLocation
+            tempLocation = santaLocation
+    print("Santa and Robo-Santa visited at least once : ",len(locationSet)," houses !")
+
+menu = input("Is Santa alone (Part 1) ? Or is he with Robo-Santa (Part2) ? ")
+if menu == "1":
+    part1()
+elif menu == "2":
+    part2()
