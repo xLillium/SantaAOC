d7d2b2b619ac1660c882324bea3112e0c1f7d6ac
xLillium <nicolas.motillon@gmail.com>
2016-01-30 21:07:25 +0100

    Fix Day1 + Add Day2 complete

---
+++ Day2.py
@@ -0,0 +1,18 @@
+# Day Two AoC : http://adventofcode.com/day/2
+paperNeeded = 0
+ribbonNeeded = 0
+inputFile = open("InputDay2")
+for box in inputFile:
+    boxDimensions = list(map(int,box.split("x")))
+    l = boxDimensions[0]
+    w = boxDimensions[1]
+    h = boxDimensions[2]
+    boxArea = 2*l*w + 2*w*h + 2*h*l
+    boxDimensions.sort()
+    slackArea = boxDimensions[0] * boxDimensions[1]
+    ribbonArea = 2*boxDimensions[0] + 2*boxDimensions[1]
+    bowArea = l * w * h
+    paperNeeded += boxArea + slackArea
+    ribbonNeeded += ribbonArea + bowArea
+inputFile.close()
+print("The elves should order : ",paperNeeded," square feet of wrapping paper and ",ribbonNeeded," square feet of ribbon.")
